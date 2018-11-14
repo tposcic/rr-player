e61e0ec3c48864bce09c4c120f09e8f224e3e756
Toni <tposcic@riteh.hr>
2018-11-14 15:47:50 +0100

    formatting changes, changes in html

--- app/assets/js/main.js
+++ app/assets/js/main.js
@@ -1,5 +1,5 @@
-class App{
-    constructor(){
+class App {
+    constructor() {
         let a = this;
         this.options = reader.settings.options;
         this.currentTrack = null;
@@ -8,13 +8,13 @@ class App{
         this.timerDisplay = null;
         this.currentTrackMetadata = {};
 
-        document.addEventListener("DOMContentLoaded", function(){
+        document.addEventListener("DOMContentLoaded", function () {
             a.initPlayer();
         });
 
-        document.addEventListener("metadataChange", function(){
+        document.addEventListener("metadataChange", function () {
             a.showMetadata();
-            if(a.options.notifications){
+            if (a.options.notifications) {
                 a.showNotification(a);
             }
         });
@@ -22,16 +22,16 @@ class App{
     /**
      * Initialize the player
      */
-    initPlayer(){
+    initPlayer() {
         let a = this;
         a.player = document.getElementById('player');
         a.setDefaults();
         a.seeker = document.getElementById('seeker');
         a.timerDisplay = document.getElementById('timerDisplay');
 
-        a.player.addEventListener('ended', function() {
-            if(a.options.autoplay){
-                if(a.options.replay){
+        a.player.addEventListener('ended', function () {
+            if (a.options.autoplay) {
+                if (a.options.replay) {
                     a.play(a.currentTrack);
                 } else {
                     a.playRandom();
@@ -39,10 +39,10 @@ class App{
             }
         }, true);
 
-        a.player.ontimeupdate = function(){
-            if(!isNaN(a.player.currentTime) && !isNaN(a.player.duration)){
-                a.seeker.value = a.player.currentTime/a.player.duration;
-                a.timerDisplay.innerHTML = a.toHHMMSS(a.player.currentTime)+' / '+a.toHHMMSS(a.player.duration);
+        a.player.ontimeupdate = function () {
+            if (!isNaN(a.player.currentTime) && !isNaN(a.player.duration)) {
+                a.seeker.value = a.player.currentTime / a.player.duration;
+                a.timerDisplay.innerHTML = a.toHHMMSS(a.player.currentTime) + ' / ' + a.toHHMMSS(a.player.duration);
                 //win.setProgressBar(a.seeker.value);
             } else {
                 a.seeker.value = 0;
@@ -50,11 +50,11 @@ class App{
                 //win.setProgressBar(0);
             }
         }
-        a.seeker.onclick = function(e){
+        a.seeker.onclick = function (e) {
             a.player.currentTime = ((e.pageX - this.offsetLeft) * this.max / this.offsetWidth) * a.player.duration;
         }
     }
-    setDefaults(){
+    setDefaults() {
         this.options.autoplay = JSON.parse(localStorage.getItem('autoplay')) || true;//not working yet, fix pls
         document.getElementById('autoplayToggle').checked = this.options.autoplay;//not working yet, fix pls
         //this.replay = input.checked;
@@ -66,21 +66,21 @@ class App{
      * Request a track
      * @param {*} sourceCallback 
      */
-    request(){
+    request() {
         let a = this;
 
         let track = reader.random();
 
         fetch(track).then(res => res.blob()).then(blob => {
             window.jsmediatags.read(blob, {
-                onSuccess: function(tag) {
+                onSuccess: function (tag) {
                     let event = new Event('metadataChange');
                     a.currentTrackMetadata = tag;
                     document.dispatchEvent(event);
                 },
-                onError: function(error) {
+                onError: function (error) {
                     let event = new Event('metadataChange');
-                    a.currentTrackMetadata = {tags:{title:'Not found',album:'Dunno',artist:'Mr. Unknown'}};
+                    a.currentTrackMetadata = { tags: { title: 'Not found', album: 'Dunno', artist: 'Mr. Unknown' } };
                     document.dispatchEvent(event);
                     console.warn(error);
                 }
@@ -92,33 +92,33 @@ class App{
     /**
      * Show current track metadata
      */
-    showMetadata(){
-        document.getElementById('trackInfo').innerHTML = 
-        '<h3 class="trackMetaSnippet artistTitle"><span>'+this.currentTrackMetadata.tags.artist+'</span></h3>'+
-        '<h3 class="trackMetaSnippet trackTitle"><span>'+this.currentTrackMetadata.tags.title +'</span></h3>'+
-        '<h3 class="trackMetaSnippet albumTitle"><span>'+this.currentTrackMetadata.tags.album+'</span></h3>';
+    showMetadata() {
+        document.getElementById('trackInfo').innerHTML =
+            '<h3 class="trackMetaSnippet artistTitle"><span>' + this.currentTrackMetadata.tags.artist + '</span></h3>' +
+            '<h3 class="trackMetaSnippet trackTitle"><span>' + this.currentTrackMetadata.tags.title + '</span></h3>' +
+            '<h3 class="trackMetaSnippet albumTitle"><span>' + this.currentTrackMetadata.tags.album + '</span></h3>';
     }
     /**
      * Show notification of the current track playing
      * @param {App} a 
      */
-    showNotification(a){
-        if (Notification.permission !== "granted"){
+    showNotification(a) {
+        if (Notification.permission !== "granted") {
             Notification.requestPermission();
         } else {
             let notification = new Notification('Now playing', {
                 icon: './assets/img/notification_button.png',
-                body: a.currentTrackMetadata.tags.artist+' - '+a.currentTrackMetadata.tags.title,
+                body: a.currentTrackMetadata.tags.artist + ' - ' + a.currentTrackMetadata.tags.title,
                 requireInteraction: false
             });
 
-            notification.onclick = function(event) {
+            notification.onclick = function (event) {
                 event.preventDefault();
                 a.playRandom();
                 notification.close();
             }
 
-            setTimeout(function() {
+            setTimeout(function () {
                 notification.close();
             }, 5000);
         }
@@ -126,14 +126,14 @@ class App{
     /**
      * Startup the random player
      */
-    playRandom(){
+    playRandom() {
         this.request();
     }
     /**
      * Play a track
      * @param {*} track 
      */
-    play(track){
+    play(track) {
         let player = document.getElementById('player');
         player.src = window.URL.createObjectURL(track);
         player.play();
@@ -142,14 +142,14 @@ class App{
      * Console log a message
      * @param {*} message 
      */
-    log(message){
+    log(message) {
         console.log(message);
     }
     /**
      * Toggle autoplaying
      * @param {HTMLElement} input 
      */
-    toggleAutoplay(input){
+    toggleAutoplay(input) {
         this.options.autoplay = input.checked;
         localStorage.setItem('autoplay', input.checked);
     }
@@ -157,7 +157,7 @@ class App{
      * Toggle replaying of the current track
      * @param {HTMLElement} input 
      */
-    toggleReplay(input){
+    toggleReplay(input) {
         this.options.replay = input.checked;
         localStorage.setItem('replay', input.checked);
     }
@@ -165,7 +165,7 @@ class App{
      * Change the volume of the player
      * @param {HTMLElement} input 
      */
-    changeVolume(input){
+    changeVolume(input) {
         let player = document.getElementById('player');
         player.volume = input.value;
         localStorage.setItem('volume', input.value);
@@ -174,9 +174,9 @@ class App{
      * toggle player
      * @param {HTMLElement} element 
      */
-    togglePlay(element){
+    togglePlay(element) {
         let player = document.getElementById('player');
-        if (player.paused){
+        if (player.paused) {
             player.play();
             element.innerHTML = '<i class="flaticon-pause"></i>';
         } else {
@@ -190,19 +190,19 @@ class App{
      */
     toHHMMSS(time) {
         var sec_num = parseInt(time, 10); // don't forget the second param
-        var hours   = Math.floor(sec_num / 3600);
+        var hours = Math.floor(sec_num / 3600);
         var minutes = Math.floor((sec_num - (hours * 3600)) / 60);
         var seconds = sec_num - (hours * 3600) - (minutes * 60);
-    
-        if(hours > 0){
-            if (hours   < 10) {hours   = "0"+hours+':';}
+
+        if (hours > 0) {
+            if (hours < 10) { hours = "0" + hours + ':'; }
         } else {
             hours = '';
         }
-        
-        if (minutes < 10) {minutes = "0"+minutes;}
-        if (seconds < 10) {seconds = "0"+seconds;}
-        return hours+minutes+':'+seconds;
+
+        if (minutes < 10) { minutes = "0" + minutes; }
+        if (seconds < 10) { seconds = "0" + seconds; }
+        return hours + minutes + ':' + seconds;
     }
 }
 
@@ -210,13 +210,18 @@ const a = new App;
 const remote = require('electron').remote
 //const win = remote.BrowserWindow;
 
-const startText = ["My body is ready", "Just click it!", "Are you oiled up properly?", "Just do it!", "Clickity click", "The hills are alive with the sound of..."];
+const startText = [
+    "My body is ready", "Just click it!", "Are you oiled up properly?", "Just do it!",
+    "Clickity click", "The hills are alive with the sound of...", "Why do i have to click this?"
+];
 
-// Bind click and change events
-document.addEventListener("DOMContentLoaded", function(){
+document.addEventListener("DOMContentLoaded", function () {
+    // this whole start button thing needs to go
+    // add this to main process -> app.commandLine.appendSwitch('--autoplay-policy','no-user-gesture-required')
+    // remove the entire start button thing after
     let startButton = document.getElementById('start');
 
-    startButton.onclick = function() {
+    startButton.onclick = function () {
         a.playRandom();
         //remove the start button once clicked
         let wrapper = this.parentNode;
@@ -225,27 +230,27 @@ document.addEventListener("DOMContentLoaded", function(){
     // set random text on the start button
     startButton.innerHTML = startText[Math.floor(Math.random() * startText.length)];
 
-    document.getElementById('random').onclick = function() {
+    document.getElementById('random').onclick = function () {
         a.playRandom();
     };
-    document.getElementById('closeAppButton').onclick = function() {
+    document.getElementById('closeAppButton').onclick = function () {
         let w = remote.getCurrentWindow()
         w.close()
     };
-    document.getElementById('minimizeAppButton').onclick = function() {
+    document.getElementById('minimizeAppButton').onclick = function () {
         let w = remote.getCurrentWindow()
         w.minimize()
     };
-    document.getElementById('playToggle').onclick = function() {
+    document.getElementById('playToggle').onclick = function () {
         a.togglePlay(this);
     };
-    document.getElementById('autoplayToggle').onchange = function() {
+    document.getElementById('autoplayToggle').onchange = function () {
         a.toggleAutoplay(this);
     }
-    document.getElementById('replayToggle').onchange = function() {
+    document.getElementById('replayToggle').onchange = function () {
         a.toggleReplay(this);
     }
-    document.getElementById('volumeRange').onchange = function(){
+    document.getElementById('volumeRange').onchange = function () {
         a.changeVolume(this);
     }
 });
