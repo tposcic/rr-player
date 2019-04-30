8e6b5c38ea96c4c27b41a6ef7a7a43422fe6ad5b
Toni <tposcic@riteh.hr>
2019-04-30 22:09:14 +0200

    added saving favorites and playing tracks from the favorites list

--- app/assets/js/main.js
+++ app/assets/js/main.js
@@ -1,21 +1,24 @@
 class App {
     constructor() {
-        let a = this;
         this.options = reader.settings.options;
         this.currentTrack = null;
+        this.currentTrackPath = null;
         this.player = null;
         this.seeker = null;
+        this.favorites = JSON.parse(localStorage.getItem('favorites')) || [];
         this.timerDisplay = null;
         this.currentTrackMetadata = {};
 
-        document.addEventListener("DOMContentLoaded", function () {
-            a.initPlayer();
+        document.addEventListener("DOMContentLoaded", () => {
+            this.initPlayer();
+            this.generateFavoritesList();
         });
 
-        document.addEventListener("metadataChange", function () {
-            a.showMetadata();
-            if (a.options.notifications) {
-                a.showNotification(a);
+        document.addEventListener("metadataChange", () => {
+            this.showMetadata();
+            this.favoriteStatusDisplay();
+            if (this.options.notifications) {
+                this.showNotification(a);
             }
         });
     }
@@ -54,6 +57,7 @@ class App {
             a.player.currentTime = ((e.pageX - this.offsetLeft) * this.max / this.offsetWidth) * a.player.duration;
         }
     }
+
     setDefaults() {
         this.options.autoplay = JSON.parse(localStorage.getItem('autoplay')) || true;//not working yet, fix pls
         document.getElementById('autoplayToggle').checked = this.options.autoplay;//not working yet, fix pls
@@ -62,14 +66,14 @@ class App {
         document.getElementById('volumeRange').value = this.player.volume;
 
     }
-    /**
-     * Request a track
-     * @param {*} sourceCallback 
-     */
-    request() {
-        let a = this;
 
-        let track = reader.random();
+    /**
+     * Request a track by path
+     * @param {String} track 
+     */
+    request(track) {
+        let a = this;
+        this.currentTrackPath = track;
 
         fetch(track).then(res => res.blob()).then(blob => {
             window.jsmediatags.read(blob, {
@@ -89,6 +93,7 @@ class App {
             a.play(blob);
         });
     }
+
     /**
      * Show current track metadata
      */
@@ -98,6 +103,15 @@ class App {
             '<h3 class="trackMetaSnippet trackTitle"><span>' + this.currentTrackMetadata.tags.title + '</span></h3>' +
             '<h3 class="trackMetaSnippet albumTitle"><span>' + this.currentTrackMetadata.tags.album + '</span></h3>';
     }
+
+    favoriteStatusDisplay() {
+        if(this.checkIfFavorite()){
+            document.getElementById('favoriteControl').classList.add('active');
+        } else {
+            document.getElementById('favoriteControl').classList.remove('active');
+        }
+    }
+
     /**
      * Show notification of the current track playing
      * @param {App} a 
@@ -123,12 +137,22 @@ class App {
             }, 5000);
         }
     }
+
     /**
      * Startup the random player
      */
     playRandom() {
-        this.request();
+        let track = reader.random();
+        this.request(track);
     }
+
+    /**
+     * Play a preselected track
+     */
+    playSelected(track) {
+        this.request(track);
+    }
+
     /**
      * Play a track
      * @param {*} track 
@@ -138,6 +162,7 @@ class App {
         player.src = window.URL.createObjectURL(track);
         player.play();
     }
+
     /**
      * Console log a message
      * @param {*} message 
@@ -145,6 +170,7 @@ class App {
     log(message) {
         console.log(message);
     }
+
     /**
      * Toggle autoplaying
      * @param {HTMLElement} input 
@@ -161,6 +187,98 @@ class App {
         this.options.replay = input.checked;
         localStorage.setItem('replay', input.checked);
     }
+
+    /**
+     * Toggle a favorite track from the favorite list
+     */
+    toggleFavorite(){
+        let track = {
+            path: this.currentTrackPath,
+            artist: this.currentTrackMetadata.tags.artist,
+            title: this.currentTrackMetadata.tags.title,
+            album: this.currentTrackMetadata.tags.album
+        }
+
+        let existingIndex = this.checkIfFavorite();
+
+        if(existingIndex){
+            this.favorites.splice(existingIndex, 1);
+        } else {
+            this.favorites.push(track);
+        }
+
+        localStorage.setItem('favorites', JSON.stringify(this.favorites));
+        this.favoriteStatusDisplay();
+        this.generateFavoritesList();
+    }
+
+    /**
+     * Toggle a list of favorite tracks
+     */
+    toggleFavoriteList() {
+        let favoritesList = document.getElementById('favoritesList');
+        favoritesList.classList.toggle('active');
+    }
+
+    /**
+     * Generate the list of favorite tracks
+     */
+    generateFavoritesList(element = false){
+        element = !element ? document.getElementById('favoritesList') : element;
+
+        let favoritesElements = element.getElementsByClassName('favoriteElement');
+
+        //we have to remove the previous click handlers
+        for(let i = 0; i<favoritesElements.length; i++){
+            favoritesElements[i].onclick = '';
+        }
+
+        let html = '<ul>';
+
+        this.favorites.forEach(element => {
+            html += "<li data-path='"+element.path+"' class='favoriteElement'>"+element.title+"</li>";
+        });
+
+        html += '</ul>';
+
+        element.innerHTML = html;
+        favoritesElements = element.getElementsByClassName('favoriteElement');
+
+        //add the new click handlers
+        for(let i = 0; i<favoritesElements.length; i++){
+            favoritesElements[i].onclick = () => {
+                this.playSelected(favoritesElements[i].dataset.path);
+                element.classList.remove('active');
+            };
+        }
+    }
+
+    /**
+     * Check if a track exists in the favorites list
+     * if yes returns index
+     * if no returns false
+     * @param {Array} track 
+     */
+    checkIfFavorite(track = null){
+        let currentTrack;
+        if(track === null){
+            currentTrack = {
+                path: this.currentTrackPath,
+                artist: this.currentTrackMetadata.tags.artist,
+                title: this.currentTrackMetadata.tags.title,
+                album: this.currentTrackMetadata.tags.album
+            }
+        } else {
+            currentTrack = track;
+        }
+
+        var index = this.favorites.findIndex(fav => {
+            return fav.artist === currentTrack.artist && fav.title == currentTrack.title && fav.album == currentTrack.album ? true : false;
+        });
+        
+        return index === -1 ? false : index;
+    }
+
     /**
      * Change the volume of the player
      * @param {HTMLElement} input 
@@ -219,16 +337,15 @@ document.addEventListener("DOMContentLoaded", function () {
     // this whole start button thing needs to go
     // add this to main process -> app.commandLine.appendSwitch('--autoplay-policy','no-user-gesture-required')
     // remove the entire start button thing after
-    let startButton = document.getElementById('start');
-
-    startButton.onclick = function () {
-        a.playRandom();
-        //remove the start button once clicked
-        let wrapper = this.parentNode;
-        wrapper.parentNode.removeChild(wrapper);
-    };
+    // let startButton = document.getElementById('start');
+    // startButton.onclick = function () {
+    //     a.playRandom();
+    //     //remove the start button once clicked
+    //     let wrapper = this.parentNode;
+    //     wrapper.parentNode.removeChild(wrapper);
+    // };
     // set random text on the start button
-    startButton.innerHTML = startText[Math.floor(Math.random() * startText.length)];
+    //startButton.innerHTML = startText[Math.floor(Math.random() * startText.length)];
 
     document.getElementById('random').onclick = function () {
         a.playRandom();
@@ -244,13 +361,19 @@ document.addEventListener("DOMContentLoaded", function () {
     document.getElementById('playToggle').onclick = function () {
         a.togglePlay(this);
     };
+    document.getElementById('favoriteControl').onclick = function () {
+        a.toggleFavorite(this);
+    };
+    document.getElementById('favoriteControl').oncontextmenu = function () {
+        a.toggleFavoriteList(this);
+    };
     document.getElementById('autoplayToggle').onchange = function () {
         a.toggleAutoplay(this);
-    }
+    };
     document.getElementById('replayToggle').onchange = function () {
         a.toggleReplay(this);
-    }
+    };
     document.getElementById('volumeRange').onchange = function () {
         a.changeVolume(this);
-    }
+    };
 });
